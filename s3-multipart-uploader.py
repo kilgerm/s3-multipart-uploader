41d8d738a36e32023d3d9100156c736a1a257de0
Martin K <mkcode380@gmail.com>
2021-02-13 22:53:36 +0100

    Check for existing

--- s3-multipart-uploader.py
+++ s3-multipart-uploader.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env pytyhon3
+from __future__ import annotations
+
 import hashlib
 import logging
 import math
 import sys
+import time
 from argparse import ArgumentParser, Namespace
+from dataclasses import dataclass
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from typing import Optional
 
 import boto3
 
@@ -25,14 +30,55 @@ def parse_args() -> Namespace:
     subparsers = parser.add_subparsers(dest="operation")
 
     start_parser = subparsers.add_parser("start")
+    start_parser.add_argument("--upload_id", metavar="UPLOAD_ID", type=str, default="",
+                              help="UploadId from previously started upload")
 
     abort_parser = subparsers.add_parser("abort")
-    #abort_parser.add_argument("bucket", type=str, help="Destination bucket name")
-    #abort_parser.add_argument("key", type=str, help="Destination key in the bucket")
-    abort_parser.add_argument("upload_id", metavar="UPLOAD_ID", type=str, help="UploadId from previously started upload")
-
+    # abort_parser.add_argument("bucket", type=str, help="Destination bucket name")
+    # abort_parser.add_argument("key", type=str, help="Destination key in the bucket")
+    abort_parser.add_argument("upload_id", metavar="UPLOAD_ID", type=str,
+                              help="UploadId from previously started upload")
 
     return parser.parse_args()
+
+
+class ProgressMeter:
+    def __init__(self, label: str, limit: int, auto_status_secs: Optional[int] = 1):
+        self.label = label
+        self.limit = limit
+        self.current = 0
+        self.auto_status_secs = auto_status_secs
+        self.start_time = time.perf_counter()
+        self.last_update_time = self.start_time
+
+    def increment(self, by: int = 1):
+        self.current = min(self.current + by, self.limit)
+        delta = time.perf_counter() - self.last_update_time
+        self.last_update_time = time.perf_counter()
+        if delta > self.auto_status_secs:
+            self.log_status()
+
+    def log_status(self):
+        percent = 100 * self.current / self.limit
+        time_passed_secs = self.last_update_time - self.start_time
+        estimated_remaining_time_secs = (
+                                                self.limit - self.current) / self.current * time_passed_secs if self.current > 0 else -1
+        LOG.info(f"{self.label} {percent:.4f}% "
+                 f"(elapsed: {int(time_passed_secs)}s, ~ETA: {int(estimated_remaining_time_secs)}s)"
+                 f"‚ ({self.current}/{self.limit})")
+
+    def __enter__(self) -> ProgressMeter:
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.log_status()
+
+
+@dataclass
+class MultipartUploadInProgress:
+    started_at: str
+    upload_id: str
+    initiator: str
 
 
 class S3MultipartUploader:
@@ -51,7 +97,47 @@ class S3MultipartUploader:
         self.args = args
         self.s3 = boto3.client("s3")
 
+    def check_existing_uploads(self) -> Optional[MultipartUploadInProgress]:
+        response = self.s3.list_multipart_uploads(
+            Bucket=self.bucket
+        )
+        LOG.debug(response)
+        matching = [MultipartUploadInProgress(
+            upload_id=upload["UploadId"],
+            started_at=upload["Initiated"],
+            initiator=upload["Initiator"]["ID"])
+            for upload in response.get("Uploads", []) if upload["Key"] == self.dest_key]
+        uploads_for_key = len(matching)
+
+        upload_id = self.args.upload_id
+        if upload_id:
+            matching = [mpupload for mpupload in matching if mpupload.upload_id == upload_id]
+
+        num_matches = len(matching)
+        if num_matches == 0:
+            if upload_id:
+                LOG.error(
+                    f"No upload found for upload_id {upload_id} (found {uploads_for_key} total uploads for this bucket and key)")
+                exit(1)
+            else:
+                LOG.info(f"No uploads found for this bucket and key")
+                return None
+        if num_matches > 1:
+            if upload_id:
+                LOG.error(f"Found {num_matches} uploads for this upload_id - this is probably a bug")
+            else:
+                LOG.error(f"Found {num_matches} uploads in progress, please specify an upload_id")
+            exit(1)
+        match = matching[0]
+        LOG.info(f"Found existing upload started at {match.started_at} by {match.initiator}, upload_id: \"{match.upload_id}\"")
+        return match
+
     def start(self):
+        upload = self.check_existing_uploads()
+        if upload:
+            self.continue_upload(upload)
+            return
+
         LOG.info(f"Start multipart upload for {self.filepath_to_upload} to s3://{self.bucket}/{self.dest_key}")
 
         part_count = math.ceil(self.filepath_to_upload.stat().st_size / self.part_size_in_bytes)
@@ -60,20 +146,23 @@ class S3MultipartUploader:
         LOG.info("Computing md5...")
         md5 = self.compute_md5(self.filepath_to_upload)
         LOG.info(f"{md5=}")
-
         filesize = self.filepath_to_upload.stat().st_size
-#        with TemporaryDirectory(prefix="s3-multipart-uploader-") as tmpdir:
-#            LOG.info(f"tempdir: {tmpdir}")
+        #        with TemporaryDirectory(prefix="s3-multipart-uploader-") as tmpdir:
+        #            LOG.info(f"tempdir: {tmpdir}")
+        exit(1)
 
         create_response = self.s3.create_multipart_upload(
             Bucket=self.bucket,
             Key=self.dest_key,
-            Metadata={"md5": md5},
+            Metadata={"md5": md5,
+                      "original_filename": self.filepath_to_upload.name
+                      },
         )
         LOG.debug(create_response)
 
         upload_id = create_response["UploadId"]
         LOG.info(f"Got {upload_id=}")
+        exit(1)
 
         for part_index in range(0, part_count):
             start_offset = min(part_index * self.part_size_in_bytes, filesize)
@@ -83,14 +172,17 @@ class S3MultipartUploader:
     @staticmethod
     def compute_md5(filepath_to_upload: Path):
         hasher = hashlib.md5()
+        file_size = filepath_to_upload.stat().st_size
         with filepath_to_upload.open("rb") as file:
-            while r := file.read(BLOCKSIZE_FOR_HASHING):
-                hasher.update(r)
+            with ProgressMeter("Computing file hash", limit=file_size) as progress:
+                while r := file.read(BLOCKSIZE_FOR_HASHING):
+                    hasher.update(r)
+                    progress.increment(BLOCKSIZE_FOR_HASHING)
         md5 = hasher.hexdigest()
         return md5
 
-    def cont(self):
-        pass
+    def continue_upload(self, upload: MultipartUploadInProgress):
+        raise Exception("Not yet implemented")
 
     def abort(self):
         print(self.s3.abort_multipart_upload(
@@ -118,7 +210,6 @@ def main():
         part_size_in_bytes=part_size,
         args=args,
     )
-
 
     s3_multipart_uploader.__getattribute__(args.operation)()
 
